50dc34428ebae2371d3da389c68ab4d3f180caf4
Pablo Perez <perez51160900@gmail.com>
2020-02-14 14:54:07 -0400

    3

--- js/app.js
+++ js/app.js
@@ -2,6 +2,13 @@ var contador  = 0;
 var punto = false;
 var valor ;
 var negativo = false;
+var signo = ""; 
+var numero1 = 0; 
+var numero2 = 0; 
+var check = false;
+
+
+
 function imprimirNumero(numero){
     
    
@@ -39,6 +46,81 @@ function Negativo(){
         document.getElementById('display').innerHTML = valor;
         negativo = false;
     }
+}
+
+function asignarSigno( valor){
+    numero1 = document.getElementById('display').innerHTML;
+    numero1 = parseFloat(numero1);
+    check = true;
+    borrarDisplay();
+    signo = valor; 
+}
+
+function ejecutarOperaciones (){
+    switch (signo){
+        case '+': suma();
+                break;
+        case '*': multiplicacion();
+                break;
+        case '-' : resta();
+                    break; 
+        case '/': division()
+            break;
+        default : break;
+    }
+}
+
+function imprimirNumero2 (numero){
+    var aux = numero;
+    if (aux < 0){
+        aux = aux *-1;
+    }
+    var cont = 0;
+     if (aux < 10 ){
+        imprimirNumero(numero.toFixed(7));
+     }else if (aux >= 10 && aux <100){
+         imprimirNumero(numero.toFixed(6));
+     }else if (aux >= 100 && aux <1000){
+        imprimirNumero(numero.toFixed(5));
+    }else if (aux >= 1000 && aux <10000){
+        imprimirNumero(numero.toFixed(4));
+    }else if (aux >= 10000 && aux <100000){
+        imprimirNumero(numero.toFixed(3));
+    }else if (aux >= 100000 && aux <1000000){
+        imprimirNumero(numero.toFixed(2));
+    }else if (aux >= 1000000 && aux <10000000){
+        imprimirNumero(numero.toFixed(1));
+    }
+}
+
+function suma(){
+    var resultado = numero1 + numero2;
+    borrarDisplay();
+    imprimirNumero2(resultado);
+
+}
+
+function multiplicacion(){
+    var resultado = numero1 * numero2;
+    borrarDisplay();
+    imprimirNumero2(resultado);
+
+}
+
+function resta(){
+   
+        var resultado = numero1 - numero2;
+        borrarDisplay();
+        imprimirNumero2(resultado);
+    
+    
+}
+
+function division(){
+    var resultado = numero1 / numero2;
+    borrarDisplay();
+    imprimirNumero2(resultado);
+
 }
 
 document.getElementById('1').addEventListener("click", function(){
@@ -82,4 +164,28 @@ document.getElementById('punto').addEventListener("click", function(){
 
 document.getElementById('sign').addEventListener("click", function(){
     Negativo();
+});
+
+document.getElementById('mas').addEventListener("click",function(){
+    asignarSigno('+');
+
+});
+
+document.getElementById('por').addEventListener("click", function(){
+    asignarSigno('*');
+});
+
+document.getElementById('menos').addEventListener("click", function(){
+    asignarSigno('-');
+});
+
+document.getElementById('dividido').addEventListener("click", function(){
+    asignarSigno('/');
+});
+
+document.getElementById('igual').addEventListener("click",function(){
+    numero2 = document.getElementById('display').innerHTML; 
+    numero2= parseFloat(numero2); 
+    ejecutarOperaciones();
+
 });
