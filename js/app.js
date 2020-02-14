6803797d10e3abe9683937e4a3dbadc6d0b39375
Pablo Perez <perez51160900@gmail.com>
2020-02-14 12:34:49 -0400

    1

---
+++ js/app.js
@@ -0,0 +1,66 @@
+var contador  = 0; 
+var punto = false;
+function imprimirNumero(numero){
+    
+   
+    if (contador <8 && !(contador == 0 && numero == 0) && ((numero == '.' && punto == false) || (numero != '.'))){
+        if (contador == 0 && numero !='.'){
+                document.getElementById('display').innerHTML = numero; 
+                
+                
+        }else{
+            if (numero == '.'){
+                punto =true
+            }
+            document.getElementById('display').innerHTML += numero; 
+        }
+        contador ++;
+    }else if(contador >=8) {
+        alert("Solo 8 digitos por favor");
+    }
+}
+
+function borrarDisplay(){
+    document.getElementById('display').innerHTML = 0; 
+    punto = false;
+    contador = 0;
+}
+
+document.getElementById('1').addEventListener("click", function(){
+    imprimirNumero(1);
+});
+document.getElementById('0').addEventListener("click", function(){
+    imprimirNumero(0);
+});
+document.getElementById('2').addEventListener("click", function(){
+    imprimirNumero(2);
+});
+document.getElementById('3').addEventListener("click", function(){
+    imprimirNumero(3);
+});
+document.getElementById('4').addEventListener("click", function(){
+    imprimirNumero(4);
+});
+document.getElementById('5').addEventListener("click", function(){
+    imprimirNumero(5);
+});
+document.getElementById('6').addEventListener("click", function(){
+    imprimirNumero(6);
+});
+document.getElementById('7').addEventListener("click", function(){
+    imprimirNumero(7);
+});
+document.getElementById('8').addEventListener("click", function(){
+    imprimirNumero(8);
+});
+document.getElementById('9').addEventListener("click", function(){
+    imprimirNumero(9);
+});
+
+document.getElementById('on').addEventListener("click", function(){
+    borrarDisplay();
+});
+
+document.getElementById('punto').addEventListener("click", function(){
+    imprimirNumero('.');
+});
