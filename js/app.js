331297359bc10d3354234d818ad8af0ff8d22ff1
Pablo Perez <perez51160900@gmail.com>
2020-02-14 19:51:40 -0400

    final

--- js/app.js
+++ js/app.js
@@ -123,67 +123,100 @@ function division(){
 
 }
 
+function animar(id){
+  var valor = document.getElementById(id).style.height;
+  if (id != 'mas'){
+  
+    document.getElementById(id).style.height = "60px";
+  } else {
+    document.getElementById(id).style.height = "140px";
+  }
+
+    setInterval(() => {
+        document.getElementById(id).style.height = valor;
+    }, 200);
+}
+
 document.getElementById('1').addEventListener("click", function(){
+    
+   animar('1');
     imprimirNumero(1);
 });
 document.getElementById('0').addEventListener("click", function(){
+    animar('0');
     imprimirNumero(0);
 });
 document.getElementById('2').addEventListener("click", function(){
+    animar('2');
     imprimirNumero(2);
 });
 document.getElementById('3').addEventListener("click", function(){
+    animar('3');
     imprimirNumero(3);
 });
 document.getElementById('4').addEventListener("click", function(){
+    animar('4');
     imprimirNumero(4);
 });
 document.getElementById('5').addEventListener("click", function(){
+    animar('5');
     imprimirNumero(5);
 });
 document.getElementById('6').addEventListener("click", function(){
+    animar('6');
     imprimirNumero(6);
 });
 document.getElementById('7').addEventListener("click", function(){
+    animar('7');
     imprimirNumero(7);
 });
 document.getElementById('8').addEventListener("click", function(){
+    animar('8');
     imprimirNumero(8);
 });
 document.getElementById('9').addEventListener("click", function(){
+    animar('9');
     imprimirNumero(9);
 });
 
 document.getElementById('on').addEventListener("click", function(){
+    animar('on');
     borrarDisplay();
 });
 
 document.getElementById('punto').addEventListener("click", function(){
+    animar('punto');
     imprimirNumero('.');
 });
 
 document.getElementById('sign').addEventListener("click", function(){
+    animar('sign');
     Negativo();
 });
 
 document.getElementById('mas').addEventListener("click",function(){
+    animar('mas');
     asignarSigno('+');
 
 });
 
 document.getElementById('por').addEventListener("click", function(){
+    animar('por');
     asignarSigno('*');
 });
 
 document.getElementById('menos').addEventListener("click", function(){
+    animar('menos');
     asignarSigno('-');
 });
 
 document.getElementById('dividido').addEventListener("click", function(){
+    animar('dividido');
     asignarSigno('/');
 });
 
 document.getElementById('igual').addEventListener("click",function(){
+    animar('igual');
     numero2 = document.getElementById('display').innerHTML; 
     numero2= parseFloat(numero2); 
     ejecutarOperaciones();
